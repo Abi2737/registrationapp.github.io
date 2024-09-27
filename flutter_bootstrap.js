8d76f1cc494759906744c3a0fbb9313e126ffab1
Abi <4ndreiene@gmail.com>
2024-09-27 15:07:49 +0300

    new changes 5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3815020775"
+    serviceWorkerVersion: "3204952267"
   }
 });
