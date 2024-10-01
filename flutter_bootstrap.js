29459fc0f2abd323284e6608452053ff43398624
Abi <4ndreiene@gmail.com>
2024-10-01 15:17:10 +0300

    update workshops v2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "816077559"
+    serviceWorkerVersion: "2888461119"
   }
 });
